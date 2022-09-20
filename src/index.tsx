7a31b63da3072a1b1991209f853733dfac720638
Mavis2103 <taquanvku@gmail.com>
2022-09-20 19:45:47 +0700

    control Zoom

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,25 @@
 import * as React from 'react';
 import L, { CRS, LatLngBounds, Icon } from 'leaflet';
 import mapData from './pangea.json';
-import vector from './dcm.json';
+import vector from './1M.json';
 import { createRoot } from 'react-dom/client';
-import { GeoJSON, MapContainer, Polygon, useMap, useMapEvents, TileLayer, Marker, Popup, ImageOverlay } from 'react-leaflet';
+import { GeoJSON, MapContainer, Polygon, useMap, useMapEvents, TileLayer, Marker, Popup, ImageOverlay, useMapEvent } from 'react-leaflet';
 import Back from './Back';
 import * as topojson from 'topojson-client';
 import Footer from './footer';
 import './styles.css';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 
-import marker from './UI_back.png';
-
-const myIcon = new Icon({
-  iconUrl: marker,
-  iconSize: [32, 32]
-});
+// const myIcon = new Icon({
+//   iconUrl: marker,
+//   iconSize: [32, 32]
+// });
 
 const binStyle = {
-  // fillColor: '#002E5E',
-  // fillOpacity: 1,
+  fillColor: '#fff',
+  fillOpacity: 1,
   // color: '#000',
-  weight: 2
+  weight: 1
 };
 const useData = json => {
   const [data, setData] = React.useState(null);
@@ -38,73 +36,26 @@ const useData = json => {
   return data;
 };
 function App() {
-  const data = useData(mapData);
+  const [isFarmMode, setIsFarmMode] = React.useState(false);
+  const [currentPoint, setCurrentPoint] = React.useState();
   const map = useMap();
-  console.log('get', map.getZoom());
-
-  // const mapEvents = useMapEvents({
-  //   zoomend: () => {
-  //     // console.log(mapEvents.getZoom());
-  //   }
-  // });
+  const mapEvents = useMapEvents({
+    zoomend: e => {
+      console.log(e);
+      setIsFarmMode(mapEvents.getZoom() === 4);
+    }
+  });
 
   const onEachCountry = (country, layer) => {
     const countryId = country.properties.OBJECTID;
-    layer.bringToBack();
-    // if (
-    //   [
-    //     1196, 1197, 1198, 1199, 1257, 1258, 1259, 1260, 1318, 1319, 1320, 1321, 1379, 1380, 1381, 1382, 1440, 1441, 1195, 1256, 1138, 1383, 1322,
-    //     1261, 1200, 1139
-    //   ].includes(country.properties.id)
-    // ) {
-    //   // layer.options.fillColor = '#FAA61A';
-    //   // layer.options.color = '#FAA61A';
-    //   layer.on('mouseover', function (e) {
-    //     layer.bindPopup(`<p> Land id : <b>${countryId}</b><p/>`).openPopup();
-    //   });
-    // } else if (
-    //   [
-    //     // 331, 332,
-    //     1181, 1182, 1242, 1243, 594, 592, 593, 532, 1302, 1241, 1304, 1742, 1863, 1679, 1739,
-    //     // 392, 393,
-    //     1740, 1741, 1801, 1802
-    //   ].includes(country.properties.id)
-    // ) {
-    //   // layer.options.fillColor = '#48C3CB';
-    //   // layer.options.color = '#48C3CB';
-    //   layer.on('mouseover', function (e) {
-    //     layer.bindPopup(`<p> Land id : <b>${countryId}</b><p/> <p> owner's name : <b>Mike Le</b><p/>`).openPopup();
-    //   });
-    // } else {
-    // layer.options.color = '#000000';
 
     layer.on({
       'click': function (e) {
-        // layer.fitBounds(e.propagatedFrom);
-        // layer.setStyle({
-        //   fillColor: '#fff'
-        // });
-        layer.bindPopup(`<p> Land id : <b>${countryId}</b><p/>`).openPopup();
+        layer.bindPopup(`<p> Land id : <b>${country.geometry.coordinates}</b><p/>`).openPopup();
       },
-      mouseover: function (e) {
-        // console.log(countryId);
-        // console.log(e.latlng);
-        // map.      }
-      },
-      zoomed: () => {
-        // console.log(mapEvents.getZoom());
-      }
+      mouseover: function (e) {},
+      zoomed: () => {}
     });
-    // console.log(country.properties);
-
-    layer.on({
-      zoomed: () => {
-        console.log('zoomed');
-
-        // console.log(mapEvents.getZoom());
-      }
-    });
-    // }
   };
   const parkIcon = new L.Icon({
     iconUrl: './UI_back.png',
@@ -116,70 +67,92 @@ function App() {
       icon: parkIcon
     });
   };
-  // React.useEffect(() => {
-  //   vector?.features.map(v => {
-  //     const latlng = v.geometry.coordinates[0][0].map(item => [item[1], item[0]]);
-  //     const image = L.imageOverlay(marker, latlng).addTo(map);
-  //   });
-  // }, [vector]);
+  console.count('map render');
+
+  const neighbor = React.useMemo(() => {
+    if (currentPoint) {
+      const k = 5;
+      const n = (k * (2 * 8 + (k - 1) * 8)) / 2 + 1;
+      const x = currentPoint;
+
+      let axis_x = [];
+      let axis_y = [];
+      Array(k)
+        .fill(0)
+        .forEach((_, k_i) => {
+          const x = Math.sqrt(n) * (k_i + 1);
+          const y = k_i + 1;
+          axis_x.push(x, -x);
+          axis_y.push(y, -y);
+        });
+      const temp = [];
+      axis_x.forEach(x => {
+        temp.push(x);
+        axis_y.forEach(y => {
+          temp.push(x + y);
+        });
+      });
+      return temp.concat(axis_y).map(C => x + C);
+    }
+  }, [currentPoint]);
+
+  const onClick = id => e => {
+    if (isFarmMode) {
+      setCurrentPoint(id);
+    }
+  };
+  const owner = [301, 190, 191, 192, 269, 230, 233, 297, 298, 302, 304, 303];
+  const myLand = [296, 298, 299];
+  const featureNearest = React.useCallback(
+    fid => {
+      // return isFarmMode ? (neighbor?.includes(fid) ? (owner.includes(fid) ? '#fff' : '#002E5E') : '#002E5E') : '#002E5E';
+      return isFarmMode ? (owner.includes(fid) ? '#ffdd00' : '#002E5E') : '#002E5E';
+    },
+    [currentPoint]
+  );
   return (
     <>
       {/* <GeoJSON data={mapData} style={binStyle} onEachFeature={onEachCountry} /> */}
-      {vector?.features.map(v => {
-        const latlng = v.geometry.coordinates[0][0].map(item => [item[1], item[0]]);
-        return (
-          <>
+      <MarkerClusterGroup>
+        {vector?.features.map(v => {
+          const latlng = v.geometry.coordinates[0][0].map(item => [item[1], item[0]]);
+          return (
             <Polygon
               key={v.properties.fid}
               positions={latlng}
+              eventHandlers={{ mouseover: onClick(v.properties.fid) }}
               pathOptions={{
-                fillColor: '#002E5E',
-                fillOpacity: [44, 111, 442, 30, 55, 56, 32].includes(v.properties.fid) ? 0 : 1,
-                weight: 2,
+                fillColor: featureNearest(v.properties.fid),
+                fillOpacity: myLand.includes(v.properties.fid) ? 0 : 1,
+                weight: 0,
                 opacity: 0
               }}>
-              {[44, 111, 442, 30, 55, 56, 32].includes(v.properties.fid) && (
-                <ImageOverlay
-                  key={v.properties.fid}
-                  url={'https://th.bing.com/th/id/R.cd8d9f8953f53182b71c7ae4c3fc5b63?rik=05wJnhix1vGwhg&pid=ImgRaw&r=0'}
-                  bounds={latlng}
-                />
-              )}
+              {myLand.includes(v.properties.fid) && <Item {...{ latlng, isFarmMode }} />}
             </Polygon>
-          </>
-        );
-      })}
-      <MarkerClusterGroup>
-        {/* <Marker position={vector.features[0].geometry.coordinates[0][0].reverse()} icon={myIcon} />
+          );
+        })}
+      </MarkerClusterGroup>
+      {/* <Marker position={vector.features[0].geometry.coordinates[0][0].reverse()} icon={myIcon} />
         <Marker position={vector.features[1].geometry.coordinates[0][0].reverse()} icon={myIcon} />
         <Marker position={vector.features[2].geometry.coordinates[0][0].reverse()} icon={myIcon} />
         <Marker position={vector.features[3].geometry.coordinates[0][0].reverse()} icon={myIcon} />
         <Marker position={vector.features[4].geometry.coordinates[0][0].reverse()} icon={myIcon} /> */}
-      </MarkerClusterGroup>
     </>
   );
 }
 
-function Test() {
-  return (
-    <>
-      <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-
-      <MarkerClusterGroup>
-        <Marker position={[49.8397, 24.0297]}>
-          <img src='./UI_back.png' alt='' />
-        </Marker>
-        <Marker position={[52.2297, 21.0122]} />
-        <Marker position={[51.5074, -0.0901]} />
-      </MarkerClusterGroup>
-    </>
+const Item = ({ latlng, isFarmMode }) => {
+  return isFarmMode ? (
+    <ImageOverlay url={'https://th.bing.com/th/id/R.cd8d9f8953f53182b71c7ae4c3fc5b63?rik=05wJnhix1vGwhg&pid=ImgRaw&r=0'} bounds={latlng} />
+  ) : (
+    <Polygon positions={latlng} pathOptions={binStyle} />
   );
-}
+};
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <MapContainer style={{ height: '100vh', width: '100%' }} zoom={0} maxZoom={5} center={[1, 1]} crs={CRS.EPSG3857}>
+  <MapContainer style={{ height: '100vh', width: '100%' }} zoom={0} maxZoom={4} center={[1, 1]} crs={CRS.EPSG3857}>
     <App />
   </MapContainer>
 );
